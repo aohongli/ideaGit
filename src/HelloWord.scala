3b02a8531c2702dfca6adb87166c8372394ae0e1
aohongli <1084243950@qq.com>
2020-03-28 15:27:12 +0800

    Initial commit

---
+++ src/HelloWord.scala
@@ -0,0 +1,9 @@
+/**
+ * @Date: 2020/2/22 17:12
+ * @Author: aohongli
+ * @Description:
+ *
+ */
+class HelloWord {
+
+}
---
+++ src/Helloword2.scala
@@ -0,0 +1,11 @@
+/**
+ * @Date: 2020/2/22 17:12
+ * @Author: aohongli
+ * @Description:
+ *
+ */
+object Helloword2 {
+  def main(args: Array[String]): Unit = {
+    pin
+  }
+}
